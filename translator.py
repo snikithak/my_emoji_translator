6defff3bef66038506a0b6b4dd4878fc5b3edaf1
Snikitha Karumudi <snikitha.karumudi@csedge.org>
2024-11-09 11:55:53 -0800

    added a way to get user input and split up the input sentence into a list of the words

--- translator.py
+++ translator.py
@@ -1,1 +1,18 @@
-print("hi")
+#IDEAS
+# map words to emoji so create input and split up that input(sentence into the words 
+#of the sentence)
+#make the words equal to something? so we can compare that word to values in a 
+#dictionary: consisting of emojis matched up with words
+
+#getting user input
+print("Enter a sentence:")
+inp = input()
+print(inp)
+
+#process of splitting up the sentence into a list of words
+sentence = inp.split()
+print ("split up: " + str(sentence))
+
+
+
+
