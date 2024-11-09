1119bbf8b8876785dfc80f92a92f5cbe4ba542b3
Snikitha Karumudi <snikitha.karumudi@csedge.org>
2024-11-09 14:44:38 -0800

    added small emoji dictionary and currently working on a loop to change the word into an emoji if it's in the dictionary

--- translator.py
+++ translator.py
@@ -4,15 +4,40 @@
 #make the words equal to something? so we can compare that word to values in a 
 #dictionary: consisting of emojis matched up with words
 
-#getting user input
+#STEP 3: create a dictionary of emojis, key = word, value = emoji
+emoji_dict = {
+    "love": "❤️",
+    "cookie": "🍪",
+    "eat": "🍽️"
+}
+
+#getting user input(WORKS)
 print("Enter a sentence:")
 inp = input()
-print(inp)
+#print(inp)
 
-#process of splitting up the sentence into a list of words
+#process of splitting up the sentence into a list of words(WORKS)
 sentence = inp.split()
-print ("split up: " + str(sentence))
+#print ("split up: " + str(sentence))
 
+#find out how to access each part of the split list and then compare that to a 
+#dictionary of emojis
+
+answer = ""
+
+for item in sentence:
+    #print(item)
+    for key in emoji_dict:
+        if(item==key):
+            answer = emoji_dict.get(key)
+        else:
+            answer += item
+
+print (answer)
+#  for key in emoji_dict:
+#     print(emoji_dict.get(key))
+
+#print(emoji_dict.values())
 
 
 
